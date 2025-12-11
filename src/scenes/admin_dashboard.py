03de4d73c069a2f27d7555597e3b0c6ae1ff2a78
Avinash Bissoon <avinash2005b@gmail.com>
2025-12-11 13:31:08 -0400

    Loading data into the View Users Table

--- src/scenes/admin_dashboard.py
+++ src/scenes/admin_dashboard.py
@@ -27,6 +27,30 @@ class AdminDashboardScene(BaseScene, UiAdminDashboard):
         self.view_users_btn.clicked.connect(lambda: self.switch_to(self.view_users_page))
         self.back_to_home_btn.clicked.connect(lambda: self.switch_to(self.admin_home_page))
 
+
+        # --- VIEW USERS TABLE SETUP
+        self.customer_table_widget.setColumnCount(5)
+        self.customer_table_widget.setHorizontalHeaderLabels(["User ID", "Name", "Email", "Phone","Role"])
+        self.load_data()
+
+    #--- LOAD DATA FROM DATABASE TO TABLE
+    def load_data(self):
+        connection = sqlite3.connect("taxibooking.db")
+        cur = connection.cursor()
+        sqlquery = "SELECT ID,FIRSTNAME, PHONENUM, EMAIL, ROLE FROM user"
+
+        self.customer_table_widget.setRowCount(100)
+        self.customer_table_widget.verticalHeader().hide()
+        tablerow = 0
+        for row in cur.execute(sqlquery):
+            self.customer_table_widget.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(row[0]))
+            self.customer_table_widget.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(row[1]))
+            self.customer_table_widget.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(row[2]))
+            self.customer_table_widget.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(row[3]))
+            self.customer_table_widget.setItem(tablerow, 4, QtWidgets.QTableWidgetItem(row[4]))
+            tablerow += 1
+        connection.close()
+
     def switch_to(self, page):
         self.refresh_scene()
         self.admin_stackedWidget.setCurrentWidget(page)
