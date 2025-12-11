bb075d32e865fe980b0f1325e26a9b7e3ec12447
Avinash Bissoon <avinash2005b@gmail.com>
2025-12-11 19:40:15 -0400

    User to Users

--- src/scenes/admin_dashboard.py
+++ src/scenes/admin_dashboard.py
@@ -47,7 +47,7 @@ class AdminDashboardScene(BaseScene, UiAdminDashboard):
     def load_data(self):
         connection = sqlite3.connect("taxibooking.db")
         cur = connection.cursor()
-        sqlquery = "SELECT ID, FIRSTNAME, PHONENUM, EMAIL, ROLE FROM user"
+        sqlquery = "SELECT ID, FIRSTNAME, PHONENUM, EMAIL, ROLE FROM users"
 
         self.customer_table_widget.setRowCount(25)
         self.driver_table_widget.setRowCount(25)
